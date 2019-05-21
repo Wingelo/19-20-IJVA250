a5eec013bc02fe6b71c6b01b7ca92410dc62a0c7
cedric.gamrat <cedric.gamrat@gmail.com>
2019-05-21 14:39:47 +0200

    Fin du tp

--- src/main/java/com/example/demo/controller/ExportController.java
+++ src/main/java/com/example/demo/controller/ExportController.java
@@ -2,13 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Client;
 import com.example.demo.entity.Facture;
+import com.example.demo.entity.LigneFacture;
 import com.example.demo.service.ClientService;
 import com.example.demo.service.FactureService;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xwpf.usermodel.VerticalAlign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -120,5 +120,114 @@ public class ExportController {
         }
         workbook.write(response.getOutputStream());
         workbook.close();
+    }
+
+    @GetMapping("/factures/xlsx")
+    public void factureXLSXAllClient(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-Disposition", "attachment; filename=\"clients.xlsx\"");
+        List<Client> allClients = clientService.findAllClients();
+        Workbook workbook = new XSSFWorkbook();
+        for (Client client : allClients) {
+            Sheet sheet = workbook.createSheet(client.getNom());
+            Row headerRow = sheet.createRow(0);
+
+            // Paramétre Nom, Prenom etc...
+            Cell cellId = headerRow.createCell(0);
+            cellId.setCellValue("Id");
+
+            Cell cellPrenom = headerRow.createCell(1);
+            cellPrenom.setCellValue("Prénom");
+
+            Cell cellNom = headerRow.createCell(2);
+            cellNom.setCellValue("Nom");
+
+            Cell cellDateDeNaissance = headerRow.createCell(3);
+            cellDateDeNaissance.setCellValue("Date de naissance");
+
+            // Détails du client
+            Row row = sheet.createRow(1);
+
+            Cell id = row.createCell(0);
+            id.setCellValue(client.getId());
+
+            Cell prenom = row.createCell(1);
+            prenom.setCellValue(client.getPrenom());
+
+            Cell nom = row.createCell(2);
+            nom.setCellValue(client.getNom());
+
+            Cell dateNaissance = row.createCell(3);
+            dateNaissance.setCellValue(client.getDateNaissance().format(DateTimeFormatter.ofPattern("dd/MM/YYYY")));
+
+            List<Facture> factures = factureService.findFacturesClient(client.getId());
+            //Factures
+            for (Facture facture : factures) {
+                Sheet sheetFacture = workbook.createSheet("Facture "+ facture.getId().toString());
+                Row headerRowFacture = sheetFacture.createRow(0);
+
+                Cell cellNomProduit = headerRowFacture.createCell(0);
+                cellNomProduit.setCellValue("Nom de produit");
+
+                Cell cellPrixProduit = headerRowFacture.createCell(1);
+                cellPrixProduit.setCellValue("Prix produit");
+
+                Cell cellQuantiteProduit = headerRowFacture.createCell(2);
+                cellQuantiteProduit.setCellValue("Quantité");
+
+                Cell cellSousTotal = headerRowFacture.createCell(3);
+                cellSousTotal.setCellValue("Sous total");
+
+                int rowFacture = 1;
+                for(LigneFacture lignefacture : facture.getLigneFactures()){
+                    Row rowF = sheetFacture.createRow(rowFacture);
+
+                    Cell cellProduit =rowF.createCell(0);
+                    cellProduit.setCellValue(lignefacture.getArticle().getLibelle());
+
+                    Cell cellPrix = rowF.createCell(1);
+                    cellPrix.setCellValue(lignefacture.getArticle().getPrix());
+
+                    Cell cellQuantite = rowF.createCell(2);
+                    cellQuantite.setCellValue(lignefacture.getQuantite());
+
+                    Cell cellSTotal = rowF.createCell(3);
+                    cellSTotal.setCellValue(lignefacture.getSousTotal());
+
+                    rowFacture = rowFacture + 1;
+                }
+                CellStyle style = workbook.createCellStyle();
+                style.setFillForegroundColor(IndexedColors.RED.getIndex());
+                style.setFillPattern(FillPatternType.forInt(PatternFormatting.SOLID_FOREGROUND));
+                Font font = workbook.createFont();
+                font.setColor(IndexedColors.WHITE.getIndex());
+                style.setAlignment(HorizontalAlignment.CENTER);
+                style.setFont(font);
+
+                CellStyle styleResultat = workbook.createCellStyle();
+                styleResultat.setFillForegroundColor(IndexedColors.RED.getIndex());
+                styleResultat.setFillPattern(FillPatternType.forInt(PatternFormatting.SOLID_FOREGROUND));
+                Font fontResultat = workbook.createFont();
+                fontResultat.setColor(IndexedColors.WHITE.getIndex());
+                styleResultat.setFont(fontResultat);
+
+                Row rowTotal = sheetFacture.createRow(rowFacture);
+
+                Cell cellTotal =rowTotal.createCell(0);
+                cellTotal.setCellValue("Total");
+
+                cellTotal.setCellStyle(style);
+                sheetFacture.addMergedRegion(new CellRangeAddress(rowFacture,rowFacture,0,2));
+                Cell cellPrixTotal = rowTotal.createCell(3);
+                cellPrixTotal.setCellValue(facture.getTotal());
+                cellPrixTotal.setCellStyle(styleResultat);
+            }
+
+
+
+        }
+        workbook.write(response.getOutputStream());
+        workbook.close();
+
     }
 }
